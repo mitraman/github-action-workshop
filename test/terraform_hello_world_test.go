f59596e89d5ddf4c693c1b56d9f7f36b4c52f358
mitraman <ronnie.mitra@gmail.com>
2020-02-11 19:30:00 +0000

    terratest hello world example which should fail

--- test/terraform_hello_world_test.go
+++ test/terraform_hello_world_test.go
@@ -2,8 +2,21 @@ package test
 
 import (
 	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/hello-world",
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "hello_world")
+	assert.Equal(t, "Hello, World!", output)
 	
 }
