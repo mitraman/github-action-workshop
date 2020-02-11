89f53af2de0a1678518ca832675066040f19e75c
mitraman <ronnie.mitra@gmail.com>
2020-02-11 22:11:46 +0000

    added aws to modules

--- test/terraform_vpc_init_test.go
+++ test/terraform_vpc_init_test.go
@@ -12,6 +12,7 @@ package test
 import (
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
