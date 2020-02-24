dee2220047bee9a3748e6cb895ec0701ba07ac50
mitraman <ronnie.mitra@gmail.com>
2020-02-24 21:43:07 +0000

    eks cluster create

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -103,3 +103,47 @@ resource "aws_api_gateway_resource" "MyDemoResource" {
 */
 
 /** k8 SETUP **/
+
+// EKS Service IAM Role
+resource "aws_iam_role" "ms-node" {
+  name = "eks-cluster-ms"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "ms-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.ms-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ms-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.ms-node.name}"
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.ms-node.arn
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ms-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.ms-AmazonEKSServicePolicy,
+  ]
+}
